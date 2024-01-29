fc552c7f8fcded9140b87c9bdd695c28dd8ae28c
cherrydub <cherrydub@gmail.com>
2024-01-29 21:27:49 +0000

    testing out navigation popout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Header({ setDark }) {
+  const [showNavigation, setShowNavigation] = useState(false);
+
+  const toggleNavigation = () => {
+    setShowNavigation((prevState) => !prevState);
+  };
+
   return (
     <div className="bg-black flex justify-between items-center">
       <div>
-        <i style={{ fontSize: "2rem" }} class="text-white las la-bars ml-2"></i>
+        <i
+          style={{ fontSize: "2rem" }}
+          className="text-white las la-bars ml-2 cursor-pointer"
+          onClick={toggleNavigation}
+        ></i>
+        {showNavigation && (
+          <div className="navigation-overlay text-white absolute bg-black w-1/2 h-1/2">
+            {/* Navigation links go here */}
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/about">About</a>
+              </li>
+              {/* Add more navigation links as needed */}
+            </ul>
+          </div>
+        )}
       </div>
       <div>
         <h1 className="text-3xl font-bold text-white mx-auto">
-          <a href="">🍒CHERRY NFT👾</a>
+          <a href="/">🍒CHERRY NFT👾</a>
         </h1>
       </div>
 
