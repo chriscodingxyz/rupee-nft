e6ffb095c924b16df09f53575f08c23ce3a1f770
cherrydub <cherrydub@gmail.com>
2024-06-19 18:23:19 +0100

    collectionitem looking a bit better now, still will make style changes

--- src/components/CollectionItem.jsx
+++ src/components/CollectionItem.jsx
@@ -11,13 +11,13 @@ const socialsObj = {
 };
 
 const marketsObj = {
-  blur: "/marketplaceLogos/blurLogo.png",
-  discord: "/marketplaceLogos/discordlogo.png",
-  etherscan: "/marketplaceLogos/etherscan.png",
-  openseapro: "/marketplaceLogos/openseaproLogo.png",
-  opensea: "/marketplaceLogos/opensealogo.png",
-  twitter: "/marketplaceLogos/twitterLogo.png",
-  uniswap: "/marketplaceLogos/uniswapLogo.png",
+  blur: "Blur",
+  discord: "Discord",
+  etherscan: "Etherscan",
+  openseapro: "OpenSea Pro",
+  opensea: "OpenSea",
+  twitter: "Twitter",
+  uniswap: "Uniswap",
 };
 
 export default function CollectionItem() {
@@ -51,118 +51,122 @@ export default function CollectionItem() {
   }
 
   return (
-    <div className="p-4" id="collectionitem">
-      <div className="flex">
-        <div className="flex-1">
-          <img
-            className="border-4 border-gray-300 rounded-full h-40 w-40"
-            src={`https://s3.amazonaws.com/cdn.nftpricefloor/projects/v1/${slug}.png?version=6`}
-            onError={(e) => {
-              e.target.src =
-                "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/SMPTE_Color_Bars.svg/200px-SMPTE_Color_Bars.svg.png";
-            }}
-            alt={itemDetails.name}
-          />
-        </div>
-        <div className="flex-1 pl-4">
-          <ul className="list-disc pl-5">
-            <li>
-              <strong>{itemDetails.name}</strong>{" "}
-              <span className="text-sm text-gray-500">({itemStats.slug})</span>
-            </li>
-            <li>Supply: {itemStats.totalSupply}</li>
-            <li>Listed: {itemStats.listedCount}</li>
-            <li>Unique Owners: {itemStats.totalOwners}</li>
-            <li>
+    <div className="p-6 max-w-6xl mx-auto">
+      <div className="flex items-center mb-6">
+        <img
+          className="border-4 border-gray-300 rounded-full h-24 w-24 md:h-32 md:w-32 object-cover mr-6"
+          src={`https://s3.amazonaws.com/cdn.nftpricefloor/projects/v1/${slug}.png?version=6`}
+          onError={(e) => {
+            e.target.src =
+              "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/SMPTE_Color_Bars.svg/200px-SMPTE_Color_Bars.svg.png";
+          }}
+          alt={itemDetails.name}
+        />
+        <div>
+          <h1 className="text-3xl font-bold">{itemDetails.name}</h1>
+          <h2 className="text-lg text-gray-600">
+            by {itemDetails.creator?.name || "Unknown"} ({itemStats.slug})
+          </h2>
+          <p className="text-lg">Rank #{itemDetails.ranking}</p>
+          <div className="mt-2 flex space-x-4">
+            {itemDetails.socialMedia?.map((platform, index) => (
               <a
-                href={`https://etherscan.io/address/${itemDetails.contract}`}
+                key={index}
+                href={platform.url}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-blue-500 underline"
+                className="text-blue-500 text-2xl"
               >
-                {itemDetails.contract.slice(0, 6)}...
-                {itemDetails.contract.slice(-4)}
-                <i className="las la-external-link-alt ml-1"></i>
+                <i
+                  className={socialsObj[platform.name.split("-").join("")]}
+                ></i>
               </a>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
+        <div>
+          <h3 className="text-xl font-bold mb-2">Collection Stats</h3>
+          <ul className="list-none space-y-1 text-lg">
+            <li>
+              <strong>Supply:</strong> {itemStats.totalSupply}
             </li>
             <li>
-              Social Media:
-              {itemDetails.socialMedia?.map((platform, index) => (
-                <span key={index} className="ml-2">
-                  <a
-                    href={platform.url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-blue-500 underline"
-                  >
-                    <i
-                      className={socialsObj[platform.name.split("-").join("")]}
-                    ></i>
-                  </a>
-                </span>
-              ))}
+              <strong>Listed:</strong> {itemStats.listedCount}
+            </li>
+            <li>
+              <strong>Unique Owners:</strong> {itemStats.totalOwners}
+            </li>
+            <li>
+              <strong>Floor (ETH):</strong>{" "}
+              {itemStats.floorInfo.currentFloorNative || "N/A"}
+            </li>
+            <li>
+              <strong>Floor (USD):</strong>{" "}
+              {itemStats.floorInfo.currentFloorUsd || "N/A"}
             </li>
           </ul>
         </div>
-      </div>
-      <div className="mt-8">
-        <ul className="list-disc pl-5">
-          <li>Ranking: {itemDetails.ranking}</li>
-          <li>Released: {itemDetails.releaseDate}</li>
-          <li>Creator: {itemDetails.creator?.name || "N/A"}</li>
-          <li>
-            Types:{" "}
-            {Array.isArray(itemDetails.types)
-              ? itemDetails.types.join(", ")
-              : itemDetails.types}
-          </li>
-          <li>Blockchain: {itemDetails.blockchain}</li>
-          <li>Total Supply: {itemDetails.totalSupply}</li>
-          <li>
-            Floor (ETH): {itemStats.floorInfo.currentFloorNative || "N/A"}
-          </li>
-          <li>Floor (USD): {itemStats.floorInfo.currentFloorUsd || "N/A"}</li>
-          {/* <li>
-            Marketplaces:
-            {itemDetails.marketplaces?.map((marketplace, index) => (
-              <span key={index} className="ml-2">
+        <div>
+          <h3 className="text-xl font-bold mb-2">Additional Info</h3>
+          <ul className="list-none space-y-1 text-lg">
+            <li>
+              <strong>Released:</strong> {itemDetails.releaseDate}
+            </li>
+            <li>
+              <strong>Blockchain:</strong> {itemDetails.blockchain}
+            </li>
+            <li>
+              <strong>Types:</strong>{" "}
+              {Array.isArray(itemDetails.types)
+                ? itemDetails.types.join(", ")
+                : itemDetails.types}
+            </li>
+            {itemDetails.bestPriceUrl && (
+              <li>
                 <a
-                  href={marketplace.url}
+                  href={itemDetails.bestPriceUrl}
                   target="_blank"
                   rel="noopener noreferrer"
                   className="text-blue-500 underline"
                 >
-                  <img
-                    src={marketsObj[marketplace.name]}
-                    alt={marketplace.name}
-                    title={marketplace.name}
-                    className="inline h-6 w-6 ml-1"
-                  />
+                  Lowest Priced URL{" "}
                   <i className="las la-external-link-alt ml-1"></i>
                 </a>
-              </span>
-            ))}
-          </li> */}
-          {itemDetails.bestPriceUrl && (
-            <li>
-              <a
-                href={itemDetails.bestPriceUrl}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-500 underline"
-              >
-                Lowest Priced URL{" "}
-                <i className="las la-external-link-alt ml-1"></i>
-              </a>
-            </li>
-          )}
-          {itemDetails.mintPriceEth && itemDetails.mintPriceUsd && (
-            <>
-              <li>Mint Price (ETH): {itemDetails.mintPriceEth}</li>
-              <li>Mint Price (USD): {itemDetails.mintPriceUsd}</li>
-            </>
-          )}
-        </ul>
+              </li>
+            )}
+            {itemDetails.mintPriceEth && itemDetails.mintPriceUsd && (
+              <>
+                <li>
+                  <strong>Mint Price (ETH):</strong> {itemDetails.mintPriceEth}
+                </li>
+                <li>
+                  <strong>Mint Price (USD):</strong> {itemDetails.mintPriceUsd}
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
+      </div>
+
+      <div className="border-t pt-4">
+        <h3 className="text-xl font-bold mb-2">Official Links</h3>
+        <div className="flex space-x-4">
+          {itemDetails.marketplaces?.map((marketplace, index) => (
+            <a
+              key={index}
+              href={marketplace.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 underline"
+            >
+              {marketsObj[marketplace.name]}{" "}
+              <i className="las la-external-link-alt"></i>
+            </a>
+          ))}
+        </div>
       </div>
     </div>
   );
